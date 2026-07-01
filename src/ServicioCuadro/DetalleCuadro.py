1406d56356e415d6827bafce18d9e98d2f5d7e28
Luisillo <luismario07.hr@gmail.com>
2026-06-30 20:33:09 -0600

    Commit de cambios: Se hicieron cambios a la clase Detalle cuadro apra agregar algunos metodos y se cambiaron las clases Gerente y Sucursal para mantener orden

--- src/ServicioCuadro/DetalleCuadro.py
+++ src/ServicioCuadro/DetalleCuadro.py
@@ -2,7 +2,8 @@ from ServicioProductos.Productos import Producto
 from datetime import date
 
 class DetalleCuadro():
-    def __init__(self, producto: Producto, fecha: date ):
+    def __init__(self, id: str, producto: Producto, fecha: date ):
+        self.id = id
         self.producto = producto
         self.fecha = fecha #En el proximo commit este debe dejar de existir
         self._cantidad_entrada = 0.0
@@ -12,6 +13,18 @@ class DetalleCuadro():
         self._cantidad_finao = 0.0
         self._registrar_devolucion = 0.0
         self._estado = "ABIERTO"
+        
+    def __eq__(self, value: object) -> bool:
+        if not isinstance(value, type(self)):
+            return NotImplemented
+        
+        return self.id == value.id
+
+    def __hash__(self) -> int:
+        return hash(self.id)
+    
+    def __str__(self):
+        return f"Producto: {self.producto.nombre} con fecha {self.fecha}"    
 
     def _comprobar_cuadro_esta_abierto(self) -> bool:
         return self._estado == "ABIERTO"
@@ -46,3 +59,24 @@ class DetalleCuadro():
 
     def cerrar_detalle_cuadro(self) -> None:
         self._estado = "CERRADO"
+        
+    def generar_reporte(self) -> str:
+        
+        nombre_prod = self.producto.nombre 
+        
+        reporte = (
+            f"=== REPORTE DE MOVIMIENTOS ===\n"
+            f"Producto: {nombre_prod}\n"
+            f"Estado del Cuadro: {self._estado}\n"
+            f"------------------------------\n"
+            f"Entradas:       {self._cantidad_entrada:.2f}\n"
+            f"Salidas:        {self._cantidad_salida:.2f}\n"
+            f"Ventas:         {self._cantidad_vendida:.2f}\n"
+            f"Devoluciones:   {self._registrar_devolucion:.2f}\n"
+            f"Merma:          {self._cantidad_merma:.2f}\n"
+            f"------------------------------\n"
+            f"TOTAL FINAL:    {self._cantidad_finao:.2f}\n"
+            f"=============================="
+        )
+        
+        return reporte 
